9156d8fcf0d5155b2012eb0a7170029b5d48cb4c
Ahuoiza <oizabaiye@gmail.com>
2019-09-26 21:56:31 -0400

    include alt attribute added method in photocard to render an alt tag

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,17 @@ function Footer() {
   return (
     <footer className="mt4 pb4 pt4 purple">
       <p className="f6 f4-m">&copy; 2019. Code by Oiza </p>
-      <p><a href="https://ahuoizabaiye.com" className="f6 link underline-hover dark-gray f4-m">www.ahuoizabaiye.com</a></p>
+
+      <p>
+        <a 
+        href="https://ahuoizabaiye.com" 
+        className="f6 link underline-hover dark-gray f4-m"
+        target="_blank" 
+        rel="noopener noreferrer"
+        >
+        www.ahuoizabaiye.com
+        </a>
+      </p>
     </footer>
   )
 }
--- src/components/PhotoCard.jsx
+++ src/components/PhotoCard.jsx
@@ -4,35 +4,46 @@ import './styles/PhotoCard.css'
 /*View logic for each photo. Gets props from Main*/
 
 class PhotoCard extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      photo: this.props.item,
+      url: this.props.item.url
+    }
+  }
+
+  /*handler creates img alt text from image link */
+  createAltTag = () => {
+    const url = this.state.url
+
+    //searches for keyword photo in url, and cuts url after the photo/ string
+    const keyphrase = url.slice((url.indexOf('photo') + 6), url.length)
+    //create array of words in url, removing '-'. 
+    //join array into a sentence, including spaces
+    const alt = keyphrase.split('-').join(' ')
+    return alt
+  }
 
   render() {
-
-    const photo = this.props.item
-    const url = photo.url
-    const sliceTo = url.length
-    const sliceFrom = url.indexOf('photo') + 6
-    /* code for extracting photo name from url for alt tag*/
-
-    /*getting error 'cannot read property slice of undefined' */
-    const renderAltTag = (url) => {
-      const keywordArray = url.slice(sliceFrom, sliceTo)
-      return keywordArray.split('-').join(' ')
-    }
+    const photo = this.state.photo
+    const altTag = this.createAltTag()
 
     return (
       <div key={photo.id} className="mt5">
         <div className="shot mv2">
           <img 
           src={photo.src.medium} 
-          alt={url}/>
+          alt={altTag}
+          />
         </div>
 
         <p className="mt2 mt3-m lh-title f5 f4-m f3-l">
           <a 
-          href={url} 
+          href={this.state.url} 
           className="link underline-hover dark-gray" 
           target="_blank"
-          rel="noopener noreferrer">
+          rel="noopener noreferrer"
+          >
           Get Photo
           </a>
         </p>
