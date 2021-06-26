722886748c06a4c8474b50199808952f7f2e76ac
Oiza <17093818+oizabaiye@users.noreply.github.com>
2021-06-26 00:26:03 -0400

    add date

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
-import './styles/Header.css'
+import React from "react"
+import "./styles/Header.css"
+
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+const fetchDate = new Date()
+const today = days[fetchDate.getDay()]
 
 function Header() {
-  return (
-    <header className="pt6">
-      <h1 className="f1 f-subheadline-ns b">Silent Pics</h1>
-      <h3 className="f3 f1-ns">A quieter image search</h3>
-    </header>
-  )
+	return (
+		<header className='pt6'>
+			<p className='f4 tc'>Today is {today}</p>
+			<h1 className='f1 f-subheadline-ns b'>Silent Pics</h1>
+			<h3 className='f3 f1-ns'>A quieter image search</h3>
+		</header>
+	)
 }
 
 export default Header
