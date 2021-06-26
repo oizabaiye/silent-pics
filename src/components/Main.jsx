f0ffeda4f17c89b5b0562874ec14d122257de3db
Oiza <17093818+oizabaiye@users.noreply.github.com>
2021-06-26 01:15:08 -0400

    add ts to photocard, remove class comp

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,148 +1,136 @@
 /*will handle API logic and pass data to Card components */
-import React from 'react'
-import PhotoCard from './PhotoCard'
-import './styles/Main.css'
+import React from "react"
+import PhotoCard from "./PhotoCard"
+import "./styles/Main.css"
 
 /*imports hidden API key */
-import { MY_API_KEY } from '../Constants'
-
+import { MY_API_KEY } from "../Constants"
 
 class Main extends React.Component {
-  constructor() {
-    super()
-    /*in state: store photos, user entry, an error indicator & loading indicator */
-    this.state = {
-      photos: [],
-      search: '',
-      error: null,
-      isLoading: false 
-    }
-  }
+	constructor() {
+		super()
+		/*in state: store photos, user entry, an error indicator & loading indicator */
+		this.state = {
+			photos: [],
+			search: "",
+			error: null,
+			isLoading: false,
+		}
+	}
 
-  /*as user types, entry is recorded in this.state.search*/
-  handleInputChange = (event) => {
-    this.setState({
-      [event.target.name]: event.target.value
-    })
-  }
+	/*as user types, entry is recorded in this.state.search*/
+	handleInputChange = (event) => {
+		this.setState({
+			[event.target.name]: event.target.value,
+		})
+	}
 
-  /*on submit (or enter) */
-  handleInputSubmit = (event) => {
-    event.preventDefault()
-    /*ensures error state is null (clears previous error)*/
-    this.setState({
-      error: null
-    })
-    //start the loading indicator, and API call
-    this.handleLoading()
-    this.getData()
-  }
+	/*on submit (or enter) */
+	handleInputSubmit = (event) => {
+		event.preventDefault()
+		/*ensures error state is null (clears previous error)*/
+		this.setState({
+			error: null,
+		})
+		//start the loading indicator, and API call
+		this.handleLoading()
+		this.getData()
+	}
 
-  /*on submit, switch isLoading to true, and render spinner */
-  handleLoading = () => {
-    this.setState({
-      isLoading: true
-    })
-  }
+	/*on submit, switch isLoading to true, and render spinner */
+	handleLoading = () => {
+		this.setState({
+			isLoading: true,
+		})
+	}
 
-  /*data handler */
-  getData = () => {
-    let search = this.state.search
-    const url = `https://api.pexels.com/v1/search?query=${search}`
-    fetch(url, {
-      headers: {
-        Authorization: MY_API_KEY
-      }
-    })
-    .then(response => {
-      if (response.ok) {
-        return response.json()
-      }
-    })
-    .then(data => {
-      /*on receipt of response, clear <input> box, clear loading spinner, store results */
-      const photosArray = data.photos
-      if (photosArray.length === 0) {
-        this.setState({
-          photos: 'none'
-        })
-      } else {
-        this.setState({
-          photos: data.photos,
-          search: ''
-        })
-      }
-    }).catch(() => {
-      /*if there's a server issue */
-      this.setState({
-        error: 'serverError'
-      })
-    }).finally(() => {
-      this.setState({
-        isLoading: false
-      })
-    })
-  }
+	/*data handler */
+	getData = () => {
+		let search = this.state.search
+		const url = `https://api.pexels.com/v1/search?query=${search}`
+		fetch(url, {
+			headers: {
+				Authorization: MY_API_KEY,
+			},
+		})
+			.then((response) => {
+				if (response.ok) {
+					return response.json()
+				}
+			})
+			.then((data) => {
+				/*on receipt of response, clear <input> box, clear loading spinner, store results */
+				const photosArray = data.photos
+				if (photosArray.length === 0) {
+					this.setState({
+						photos: "none",
+					})
+				} else {
+					this.setState({
+						photos: data.photos,
+						search: "",
+					})
+				}
+			})
+			.catch(() => {
+				/*if there's a server issue */
+				this.setState({
+					error: "serverError",
+				})
+			})
+			.finally(() => {
+				this.setState({
+					isLoading: false,
+				})
+			})
+	}
 
+	render() {
+		{
+			/*view logic if isLoading is truthy */
+		}
+		if (this.state.isLoading) {
+			return (
+				<div className='spinner'>
+					<div className='cube1'></div>
+					<div className='cube2'></div>
+				</div>
+			)
+		}
 
-  render() {
+		return (
+			<main>
+				<div className='wrapper'>
+					{/*html form. onsubmit, call handleSubmit function */}
+					<form onSubmit={this.handleInputSubmit}>
+						<input
+							type='search'
+							name='search'
+							className='search mt3 mb2 pa2'
+							value={this.state.search}
+							onChange={this.handleInputChange}
+							placeholder='Enter a keyword'
+						/>
+						<input type='submit' value='search' className='submit' />
+					</form>
 
-    {/*view logic if isLoading is truthy */}
-    if (this.state.isLoading) {
-      return (
-        <div className="spinner">
-          <div className="cube1"></div>
-          <div className="cube2"></div>
-        </div>
-      )
-    }
-    
-    return (
-      <main>
-        <div className="wrapper">
+					{/*view logic if problem with API */}
+					{this.state.error === "serverError" && (
+						<div className='error f5 f4-m f3-l ph2'>How embarrasing. There is an error</div>
+					)}
 
-        {/*html form. onsubmit, call handleSubmit function */}
-          <form onSubmit={this.handleInputSubmit}>
-            <input 
-            type="search" 
-            name="search" 
-            className="search mt3 mb2 pa2"
-            value={this.state.search}
-            onChange={this.handleInputChange}
-            placeholder="Enter a keyword"
-            />
-            <input type="submit" value="search" className="submit"/>
-          </form>
-           
-
-          {/*view logic if problem with API */}
-          {
-            this.state.error === 'serverError' && 
-              <div className="error f5 f4-m f3-l ph2">
-                How embarrasing. There is an error
-              </div> 
-          }
-
-          {/*view logic if no API problem - render one of two options */}
-          {
-            this.state.photos === 'none' ? 
-              <div className="error f5 f4-m f3-l ph2">
-                Sorry, there are no photos matching that criteria
-              </div> 
-              :
-            this.state.photos.map(item => (
-              <PhotoCard  
-                key={item.id}
-                item={item}
-              />
-            ))
-          }
-        
-        </div> {/*closing tag for div.wrapper*/}
-      </main>
-    )
-  }
+					{/*view logic if no API problem - render one of two options */}
+					{this.state.photos === "none" ? (
+						<div className='error f5 f4-m f3-l ph2'>
+							Sorry, there are no photos matching that criteria
+						</div>
+					) : (
+						this.state.photos.map((item) => <PhotoCard key={item.id} item={item} />)
+					)}
+				</div>
+			</main>
+		)
+	}
 }
-
 
 export default Main
---
+++ src/components/PhotoCard.tsx
@@ -0,0 +1,40 @@
+import React from "react"
+import "./styles/PhotoCard.css"
+
+/*View logic for each photo. Gets props from Main*/
+
+const createAlt = (address: string) => {
+	//searches for keyword photo in url, and cuts url after the photo/ string
+	const keyphrase: string = address.slice(address.indexOf("photo") + 6, address.length)
+	//create array of words in url, removing '-'.
+	//join array into a sentence, including spaces
+	const alt: string = keyphrase.split("-").join(" ")
+	return alt
+}
+
+function PhotoCard(props: any) {
+	const photo: any = props.item
+	const url: string = props.item.url
+	const altTag: string = createAlt(url)
+
+	return (
+		<div key={photo.id} className='mt5'>
+			<div className='shot mv2'>
+				<img src={photo.src.medium} alt={altTag} />
+			</div>
+
+			<p className='mt2 mt3-m lh-title f5 f4-m f3-l'>
+				<a
+					href={url}
+					className='link underline-hover dark-gray'
+					target='_blank'
+					rel='noopener noreferrer'
+				>
+					Get Photo
+				</a>
+			</p>
+		</div>
+	)
+}
+
+export default PhotoCard
